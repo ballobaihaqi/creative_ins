c77434cb0b093984da329d38eefe1f946fabcbc0
Moh Iqbal Baihaqi <mohiqbalbaihaqi16@gmail.com>
2021-08-20 19:24:15 +0800

    send Data to PHP server

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 $(function () {
   var data_Modal = `<div class="form-floating mb-3">
-    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" />
+    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" />
     <label for="floatingInput">E-mail</label>
     </div>
 
     <div class="form-floating mb-3">
-    <input type="text" class="form-control" id="floatingInput" placeholder="Nama Lengkap" />
+    <input type="text" class="form-control" id="floatingInput" placeholder="Nama Lengkap" name="nama" />
     <label for="floatingInput">Nama Lengkap</label>
     </div>
 
     <div class="form-floating mb-3">
-    <input type="text" class="form-control" id="floatingInput" placeholder="Jl.Samping" />
+    <input type="text" class="form-control" id="floatingInput" placeholder="Jl.Samping" name="alamat" />
     <label for="floatingInput">Alamat</label>
     </div>
 
     <div class="form-floating mb-3">
-    <input type="text" class="form-control" id="floatingInput" placeholder="081234567890" />
+    <input type="text" class="form-control" id="floatingInput" placeholder="081234567890" name="telpon" />
     <label for="floatingInput">No. Telepon/WhatsApp</label>
     </div>`;
 
-  var data_packet = `<div class="form">
-    <input type="text" class="form-control packet" id="floatingInputValue" placeholder="name@example.com" disabled>
-    </div>`;
+  var data_packet = `
+    <input type="text" class="form-control packet" id="floatingInputValue" placeholder="name@example.com" name="packet" disabled>`;
 
   $(".btnPacket1").click(function () {
     $(".modal-body").html("");
